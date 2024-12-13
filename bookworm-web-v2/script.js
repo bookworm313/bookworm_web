bfea0a33c59f5696f757fbb3ef91b0ea67a1b1e8
lovroledic <lovro.ledic03@gmail.com>
2024-12-13 16:40:44 +0100

    Prepravljen dohvat knjiga tako da se mogu spremiti ID-jevi knjiga

--- bookworm-web-v2/script.js
+++ bookworm-web-v2/script.js
@@ -3,73 +3,108 @@ const submit_button = document.getElementById("submit-button");
 const content = document.getElementById("content");
 const search_results = document.getElementById("search-results");
 
-let search_value;
+const displayResult = async function()
+{
+    search_results.replaceChildren();
 
-function displayResult(docs, docs_size, num_found) {
+    const query = search_input.value;
+    const results = await fetchBooks(query, "eng", 10, 1);
+    console.log("results", results);
+
+    // Fetch failed
+    if (!results)
+        return;
+
+    const books = [];
     
-    for (let i = 0; i < docs_size; i++) {
+    // Dohvati podatke o pojedinoj knjizi
+    for (let i = 0; i < results.retrieved_count; i++)
+    {    
+        const book = await fetchBookByKey(results.book_keys[i]);
+        console.log("book", book)
         
-        const works = docs[i]; // Skup svih mogućih izdanja iste knjige
-        let edition = docs[i].editions?.docs[0]; // Jedno specifično izdanje
+        books.push(book);
+    }
 
-        if (!edition)
-            edition = works;
-        
+    // Prikaži rezultat
+    for (let i = 0; i < results.retrieved_count; i++) 
+    {   
+        const book = books[i];
+
         // Odredi URI korice
-        let key = "id";
-        let value = edition?.cover_i;
-        let size = "M";
-        const cover_uri = value ? `https://covers.openlibrary.org/b/${key}/${value}-${size}.jpg?default=false` : "https://placehold.co/80x120/ddd/9b9b9b/png?text=No\\ncover";
-
-        const work_title = works.title;
-        const edition_title = edition.title.concat(edition.subtitle? ": " + edition.subtitle : "");
-        const author_name = edition.author_name ? edition.author_name.join(", ") : works.author_name.join(", ");
-        const first_sentence = works.first_sentence ? works.first_sentence : "";
+        const size = "M";
+        const cover_uri = book.cover_id ? `https://covers.openlibrary.org/b/id/${book.cover_id}-${size}.jpg?default=false` : "https://placehold.co/80x120/ddd/9b9b9b/png?text=No\\ncover"; 
 
         const book_container = document.createElement("div");
-        book_container.setAttribute("id", i);
+        book_container.setAttribute("id", results.start + i);
         book_container.classList.add("book-container");
         book_container.innerHTML = 
             `<div class="book-cover-container">
                 <img class="book-cover" src="${cover_uri}" alt="">
             </div>
             <div class="book-info">
-                <p class="book-author">${author_name}</p>
-                <p class="book-work-title">${work_title}</p>
-                <p class="book-edition-title">${edition_title}</p>
-                <p class="book-about">${first_sentence}</p>
+                <p class="book-author">${book.authors_key}</p>
+                <p class="book-title">${book.title}</p>
+                <p class="book-about">${book.description ?? "No info"}</p>
             </div>`
-        if (work_title !== edition_title) {
-            book_container.getElementsByClassName("book-work-title")[0].style.display = "block";
-        }
         search_results.append(book_container);
     }
 }
 
-function fetchBooks(page) {
-    search_results.replaceChildren();
-
+async function fetchBooks(query, lang, limit, page)
+{
     // Odredi URI upita
-    const query = encodeURI(search_input.value);
-    const fields = encodeURI("editions,key,title,subtitle,author_name,first_sentence,cover_i");
-    const lang = "en";
-    const limit = "10";
-    const uri = `https://openlibrary.org/search.json?q=${query}&fields=${fields}&lang=${lang}e&limit=${limit}&page=${page ? page : 0}`;
+    query = encodeURI(query);
+    //const fields = encodeURI("editions,key,title,subtitle,author_name,first_sentence,cover_i,language");
+    const fields = encodeURI("editions,key");
+    const uri = `https://openlibrary.org/search.json?q=${query}&fields=${fields}&language=${lang}&limit=${limit}&page=${page != "" ? page : "0"}`;
 
     console.log(uri);
-
-    fetch(uri)
-        .then(function (response) {
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-            response.json().then(function (json) {
-                displayResult(json.docs, limit, json.num_found);
-            });
-        })
-        .catch(function (err) {
-            console.log("Fetch error: ", err);
-        });
+    try {
+        const response = await fetch(uri);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        
+        const results = {
+            query: json.q,
+            start: json.start,
+            retrieved_count: json.docs.length,
+            total_count: json.num_found,
+            book_keys: json.docs.map((work) => work?.editions?.docs?.[0].key),
+        };
+        return results;
+    } catch (err) {
+        console.log("Fetch error: ", err);
+    }
 }
 
-submit_button.addEventListener("click", fetchBooks);
+async function fetchBookByKey(key)
+{
+    // nekad sadržava authors, ali kad nema ima works[0].key preko kojeg se dobije autor
+
+    const uri = encodeURI(`https://openlibrary.org${key}.json`);
+    try {
+        const response = await fetch(uri);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+
+        const result = {
+            cover_id: json.covers?.[0],
+            authors_key: json.authors, // fetchAuthor
+            title: json.title,
+            description: json.description?.value,
+            subjects: json.subjects, // potencijalno neki drugi atribut, treba bi se koristit za žanrove
+        }
+
+        return result;
+
+    } catch (err) {
+        console.log("Fetch error: ", err);
+    }
+}
+
+submit_button.addEventListener("click", displayResult);
