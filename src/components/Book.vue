d45309c70a64091a3d1913c17c51ae17201c4c18
„klepo313” <antonio.klepo1@gmail.com>
2025-01-20 22:17:50 +0100

    dodan prikaz recenzije na knjigama ako postoje

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="book" v-if="!isRemoved" >
-        <div class="cover-container" >
-            <img :src="props.coverUri" class="image" alt="No Cover" @click="showBookInfo()"/>
+    <div class="book" v-if="!isRemoved">
+        <div class="cover-container">
+            <img v-if="props.coverUri" :src="props.coverUri" class="image" alt="No Cover" @click="showBookInfo()" />
+            <Skeleton v-else size="5rem"></Skeleton>
         </div>
         <div class="desc-container">
             <h3 class="authors">{{ props.authorNames?.join(", ") }} ({{ props.publishYear }})</h3>
             <h2 class="title">{{ props.title }}</h2>
             <h3 class="subtitle">{{ props.subtitle }}</h3>
-            <!-- Rating Stars -->
-            <div v-if="isPageDoneReading" class="rating">
-                <font-awesome-icon
-                    v-for="n in 5"
-                    :key="n"
-                    :icon="[n <= currentReview ? 'fas' : 'far', 'star']"
-                    class="icon star"
-                    @click="toggleReview(n)"
-                />
+            <!-- Rating Stars v-if="isPageDoneReading" -->
+            <div class="rating">
+                <div v-if="!isPageDoneReading && currentReview === 0">
+                    No reviews found
+                </div>
+                <font-awesome-icon v-else v-for="n in 5" :key="n" :icon="[n <= currentReview ? 'fas' : 'far', 'star']"
+                    class="icon star" @click="isPageDoneReading ? toggleReview(n) : ''" />
             </div>
         </div>
-        
+
         <!-- <div class="progress"></div> -->
         <div>
-            <font-awesome-icon icon="trash" class="icon trash" size="lg" @click="removeFromList(props.listId, props.olid)" />
+            <font-awesome-icon icon="trash" class="icon trash" size="lg"
+                @click="removeFromList(props.listId, props.olid)" />
         </div>
         <!--<div class="review">
             <span>{{ props.review }} </span>
@@ -30,8 +30,9 @@
                 optionLabel="name" placeholder="Add to lists" :disabled="storing ? true : false" />
         </div>-->
     </div>
-    
-    <Dialog v-model:visible="dialogIsVisible" modal :style="{background:'#ecdeaa', border: 'none', width: '50%', height: '70%'}">
+
+    <Dialog v-model:visible="dialogIsVisible" modal
+        :style="{ background: '#ecdeaa', border: 'none', width: '50%', height: '70%' }">
         <div class="dialog-container">
             <div class="dialog-side">
                 <div class="dialog-side-content">
@@ -43,27 +44,22 @@
             </div>
             <div class="dialog-main">
                 <Skeleton v-if="!Book" width="100%" height="30px"></Skeleton>
-                <h2 v-else>{{ Book?.authors.map((author) => author.name).join(", ")}}</h2>
+                <h2 v-else>{{ Book?.authors.map((author) => author.name).join(", ") }}</h2>
                 <Skeleton v-if="!Book" width="100%" height="50px" style="margin-top: 10px"></Skeleton>
                 <h1 v-else>{{ Book?.title }}</h1>
                 <Skeleton v-if="!Book" width="100%" height="30px" style="margin-top: 10px"></Skeleton>
                 <h2 v-else>{{ Book?.subtitle }}</h2>
                 <Skeleton v-if="!Book" width="100%" height="300px" style="margin-top: 10px"></Skeleton>
-                <p v-else>{{ Book?.description || "No description available"}}</p>
+                <p v-else>{{ Book?.description || "No description available" }}</p>
                 <div v-for="author in Book?.authors" class="author-info">
                     <h3>About {{ author.name }}</h3>
-                    <p>Born: {{ author.birth_date || "unknown"}}</p>
+                    <p>Born: {{ author.birth_date || "unknown" }}</p>
                     <p>{{ author.bio }}</p>
                 </div>
-                <div v-if="isPageDoneReading" class="rating">
-                <font-awesome-icon
-                    v-for="n in 5"
-                    :key="n"
-                    :icon="[n <= currentReview ? 'fas' : 'far', 'star']"
-                    class="icon star"
-                    @click="toggleReview(n)"
-                />
-            </div>
+                <div class="rating">
+                    <font-awesome-icon v-for="n in 5" :key="n" :icon="[n <= currentReview ? 'fas' : 'far', 'star']"
+                    class="icon star" @click="isPageDoneReading ? toggleReview(n) : ''" />
+                </div>
             </div>
         </div>
     </Dialog>
@@ -101,7 +97,7 @@ const props = defineProps({
     coverUri: String,
     review: {
         type: Number,
-        default: 0, 
+        default: 0,
     },
 })
 
@@ -110,43 +106,43 @@ const currentReview = ref(0);
 
 // Praćenje promjena u props.review
 watch(() => props.review, (newValue) => {
-  currentReview.value = newValue;  // Ažurirajte lokalnu ocjenu kad se promijeni review
+    currentReview.value = newValue;  // Ažurirajte lokalnu ocjenu kad se promijeni review
 });
 
 const toggleReview = (value) => {
-  if (currentReview.value === value) {
-    // Ako je već odabrano, resetiraj ocjenu
-    currentReview.value = 0;
-    console.log('Ocjena uklonjena');
-  } else {
-    // Postavi novu ocjenu
-    currentReview.value = value;
-    console.log(`Odabrana ocjena: ${value}`);
-  }
-  try {
-    const url = 'http://localhost:3000/review';
-    const data = {
-        bookOlid: props.olid,
-        review: currentReview.value
+    if (currentReview.value === value) {
+        // Ako je već odabrano, resetiraj ocjenu
+        currentReview.value = 0;
+        console.log('Ocjena uklonjena');
+    } else {
+        // Postavi novu ocjenu
+        currentReview.value = value;
+        console.log(`Odabrana ocjena: ${value}`);
     }
-    console.log(data);
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(data => {
+    try {
+        const url = 'http://localhost:3000/review';
+        const data = {
+            bookOlid: props.olid,
+            review: currentReview.value
+        }
         console.log(data);
-    })
-    .catch(error => {
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    } catch (error) {
         console.error(error);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+    }
 };
 
 onBeforeMount(() => {
@@ -160,19 +156,19 @@ onMounted(async () => {
     //check if the # in url is '#done_reading' and set isPageDoneReading to true
     if (window.location.hash == "#done_reading") {
         isPageDoneReading.value = true;
-
-        try {
-            console.log("OLID: ", props.olid)
-            const url = 'http://localhost:3000/review/' + props.olid;
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-            if(data[0]?.stars) { currentReview.value = data[0].stars;}
-        } catch (error) {
-            console.error(error);
-        }
-
     }
+    try {
+        console.log("OLID: ", props.olid)
+        const url = 'http://localhost:3000/review/' + props.olid;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        if (data[0]?.stars) { currentReview.value = data[0].stars; }
+    } catch (error) {
+        console.error(error);
+    }
+
+
 })
 
 async function removeFromList(listId, olid) {
@@ -180,11 +176,11 @@ async function removeFromList(listId, olid) {
     isRemoved.value = true;
 }
 
-const Book = ref(null);  
+const Book = ref(null);
 const dialogIsVisible = ref(false);
-const showBookInfo = async () => {  
+const showBookInfo = async () => {
     dialogIsVisible.value = true;
-    Book.value = await fetchBook(props.olid); 
+    Book.value = await fetchBook(props.olid);
 }
 </script>
 
@@ -215,23 +211,25 @@ const showBookInfo = async () => {
     text-align: center;
 }
 
-.desc-container {
-    
-}
+.desc-container {}
 
 .authors {
     font-size: 16px;
 }
+
 .title {
     font-size: 22px;
 }
+
 .subtitle {
     font-size: 18px;
     font-style: italic;
 }
-.descr{
+
+.descr {
     font-size: small;
 }
+
 .review {
     display: flex;
     align-items: center;
@@ -257,7 +255,7 @@ const showBookInfo = async () => {
     color: var(--gold);
 }
 
-.p-dialog .image{
+.p-dialog .image {
     width: auto;
     height: auto;
 }
@@ -269,25 +267,27 @@ const showBookInfo = async () => {
     gap: 20px;
     padding: 0 10px;
 }
+
 .dialog-side {
     position: relative;
     min-width: 200px;
     max-width: 200px;
 }
+
 .dialog-side-content {
     width: 200px;
     position: fixed;
 }
+
 .dialog-side-content img {
     width: 100%;
 }
+
 .author-info {
     margin-top: 10px;
 }
+
 .dialog-main {
     flex-grow: 1;
 }
-
-
-
 </style>
